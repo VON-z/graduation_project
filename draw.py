6fa55619ad5ba2e0c0b1d7f97a9989ffc3473ef7
VONz <1036991178@qq.com>
2022-05-12 00:39:22 +0800

    backup

--- draw.py
+++ draw.py
@@ -14,3 +14,24 @@
 # here put the third-party packages
 
 # here put the local import source
+
+if __name__ == '__main__':
+    pass
+
+# # Draw evaluation figure.
+# # 支持中文以及负数
+# plt.rcParams['font.sans-serif']=['STSong']  # SimHei黑体 STSong宋体
+# plt.rcParams['axes.unicode_minus'] = False
+
+# x = list(range(IT))
+# y = [sum(evaluation[i]) / POPULATION for i in range(IT)]
+# plt.subplot(1, 2, 1)
+# plt.plot(x, y, linewidth=2.0)
+# # plt.show()
+
+# # Draw heatmap.
+# plt.subplot(1, 2, 2)
+# sns.set_theme()
+# sns.heatmap(evaluation.T)
+# plt.show()
+# print('Breakpoint')
