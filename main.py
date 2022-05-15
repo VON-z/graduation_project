6b4d106df6d33acd4f355f478dbe89031594fd44
VONz <1036991178@qq.com>
2022-05-15 16:17:07 +0800

    delete a redundant Network parameter.

--- main.py
+++ main.py
@@ -32,8 +32,7 @@ SKILL_NUM = 5
 MAX_MEMBER_NUM = 5
 
 # Network Hyperparameters
-NETWORK_NUM = 10
-NETWORK_SCALE = [20, 60, 100]
+NETWORK_SCALE = [20, 40, 60, 80, 100]
 NETWORK_TYPE = ['ER', 'WS', 'BA']
 ER_P = [0.1, 0.5, 0.9]
 WS_P = [0.1, 0.5, 0.9]
@@ -50,57 +49,57 @@ TE = [10, 100, 500]
 ALPHA = [0.95, 0.97, 0.99]
 L = 5
 
-Agent.skill_num = SKILL_NUM # Initialize Agent class variables.  
+Agent.skill_num = SKILL_NUM # Initialize Agent class variables.
 
 # 1.Genetic Algorithm.
-# for r in trange(10):
-#     for network_scale in NETWORK_SCALE:
-#         skills_matrix = load_skills(network_scale, r)
-#         motivation = load_motivation(network_scale, r)
-#         # ER
-#         for p in ER_P:
-#             w1 = load_network(network_scale, 'ER', r=r, layer=1, p=p)
-#             w2 = load_network(network_scale, 'ER', r=r, layer=2, p=p)
-#             network = Network(network_scale, W1=w1, W2=w2,
-#                 skills=skills_matrix, motivation=motivation)    # Build the network.
-#             ga = GA(network, MAX_MEMBER_NUM, SKILL_NUM)
-#             # Execute.
-#             for pc in PC:
-#                 for pm in PM:
-#                     evaluation, best_solution, best_solution_evaluation = \
-#                         ga(C_max=C_MAX, P=POPULATION, pc=pc, pm=pm)
-#                     write2file(evaluation, best_solution, best_solution_evaluation,
-#                         r, 'GA', 'ER', network_scale, p=p, pc=pc, pm=pm)
+for r in trange(10):
+    for network_scale in NETWORK_SCALE:
+        skills_matrix = load_skills(network_scale, r)
+        motivation = load_motivation(network_scale, r)
+        # ER
+        for p in ER_P:
+            w1 = load_network(network_scale, 'ER', r=r, layer=1, p=p)
+            w2 = load_network(network_scale, 'ER', r=r, layer=2, p=p)
+            network = Network(network_scale, W1=w1, W2=w2,
+                skills=skills_matrix, motivation=motivation)    # Build the network.
+            ga = GA(network, MAX_MEMBER_NUM, SKILL_NUM)
+            # Execute.
+            for pc in PC:
+                for pm in PM:
+                    evaluation, best_solution, best_solution_evaluation = \
+                        ga(C_max=C_MAX, P=POPULATION, pc=pc, pm=pm)
+                    write2file(evaluation, best_solution, best_solution_evaluation,
+                        r, 'GA', 'ER', network_scale, p=p, pc=pc, pm=pm)
 
-#         # WS
-#         for p in WS_P:
-#             w1 = load_network(network_scale, 'WS', r=r, layer=1, p=p)
-#             w2 = load_network(network_scale, 'WS', r=r, layer=2, p=p)
-#             network = Network(network_scale, W1=w1, W2=w2,
-#                 skills=skills_matrix, motivation=motivation)    # Build the network.
-#             ga = GA(network, MAX_MEMBER_NUM, SKILL_NUM)
-#             # Execute.
-#             for pc in PC:
-#                 for pm in PM:
-#                     evaluation, best_solution, best_solution_evaluation = \
-#                         ga(C_max=C_MAX, P=POPULATION, pc=pc, pm=pm)
-#                     write2file(evaluation, best_solution, best_solution_evaluation,
-#                         r, 'GA', 'WS', network_scale, p=p, pc=pc, pm=pm)
+        # WS
+        for p in WS_P:
+            w1 = load_network(network_scale, 'WS', r=r, layer=1, p=p)
+            w2 = load_network(network_scale, 'WS', r=r, layer=2, p=p)
+            network = Network(network_scale, W1=w1, W2=w2,
+                skills=skills_matrix, motivation=motivation)    # Build the network.
+            ga = GA(network, MAX_MEMBER_NUM, SKILL_NUM)
+            # Execute.
+            for pc in PC:
+                for pm in PM:
+                    evaluation, best_solution, best_solution_evaluation = \
+                        ga(C_max=C_MAX, P=POPULATION, pc=pc, pm=pm)
+                    write2file(evaluation, best_solution, best_solution_evaluation,
+                        r, 'GA', 'WS', network_scale, p=p, pc=pc, pm=pm)
 
-#         # BA
-#         for m in BA_M:
-#             w1 = load_network(network_scale, 'BA', r=r, layer=1, m=m)
-#             w2 = load_network(network_scale, 'BA', r=r, layer=2, m=m)
-#             network = Network(network_scale, W1=w1, W2=w2,
-#                 skills=skills_matrix, motivation=motivation)    # Build the network.
-#             ga = GA(network, MAX_MEMBER_NUM, SKILL_NUM)
-#             # Execute.
-#             for pc in PC:
-#                 for pm in PM:
-#                     evaluation, best_solution, best_solution_evaluation = \
-#                         ga(C_max=C_MAX, P=POPULATION, pc=pc, pm=pm)
-#                     write2file(evaluation, best_solution, best_solution_evaluation,
-#                         r, 'GA', 'BA', network_scale, m=m, pc=pc, pm=pm)
+        # BA
+        for m in BA_M:
+            w1 = load_network(network_scale, 'BA', r=r, layer=1, m=m)
+            w2 = load_network(network_scale, 'BA', r=r, layer=2, m=m)
+            network = Network(network_scale, W1=w1, W2=w2,
+                skills=skills_matrix, motivation=motivation)    # Build the network.
+            ga = GA(network, MAX_MEMBER_NUM, SKILL_NUM)
+            # Execute.
+            for pc in PC:
+                for pm in PM:
+                    evaluation, best_solution, best_solution_evaluation = \
+                        ga(C_max=C_MAX, P=POPULATION, pc=pc, pm=pm)
+                    write2file(evaluation, best_solution, best_solution_evaluation,
+                        r, 'GA', 'BA', network_scale, m=m, pc=pc, pm=pm)
 
 
 # 2.Simulated Annealing.
